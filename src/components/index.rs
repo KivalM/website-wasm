dc2b7533b1606e893d9e7111188d6727f8911b1b
Kival M <kivalm@protonmail.com>
2023-04-02 14:08:29 +0200

    update

--- src/components/index.rs
+++ src/components/index.rs
@@ -1,111 +1,52 @@
-use crate::{components::other::particle_animation::ParticleAnimation, router::Route};
-use yew::{function_component, html, Callback, Html};
-use yew_router::prelude::use_navigator;
-
-// mutable global variable to store the mouse position
-pub static mut MOUSE_POS: (i32, i32) = (0, 0);
+use yew::{function_component, html, Html};
 
 #[function_component(IndexMainComponent)]
 pub fn index_main_component() -> Html {
-    // add a mouse move callback to update the mouse position
-    let mouse_move_callback = Callback::from(move |event: web_sys::MouseEvent| {
-        let x = event.client_x();
-        let y = event.client_y();
-
-        unsafe {
-            MOUSE_POS = (x, y);
-        }
-    });
-
-    let touch_move_callback = Callback::from(move |event: web_sys::TouchEvent| {
-        let touches = event.touches();
-        let touch = touches.get(0).unwrap();
-        let x = touch.client_x();
-        let y = touch.client_y();
-
-        unsafe {
-            MOUSE_POS = (x, y);
-        }
-    });
-
-    let navigator = use_navigator().unwrap();
-    let navigate = move |route: Route| {
-        let navigator = navigator.clone();
-        Callback::from(move |_| navigator.push(&route))
-    };
-
     html! {
-      <>
-        <div class="h-screen w-screen bg-black p-2 sm:p-8 select-none">
-            // inner box with w
-            <div class="h-full w-full border-2 border-gray-400 rounded" id="main-box"
-                onmousemove={mouse_move_callback} ontouchmove={touch_move_callback}>
+    <>
+        <header class="bg">
+            <div class="bg-black/60 bg-tras h-full w-full flex justify-center">
+                <div class="flex h-full justify-center text-center flex-col w-fit">
+                    <h1
+                        class="text-6xl comfortaa font-bold hover:scale-110 transition-all duration-100 bg-clip-text text-transparent bg-gradient-to-tr from-blue-500 to-blue-600">
+                        {"Kival Mahadew"}
+                    </h1>
+                    <h2
+                        class="text-2xl comfortaa font-bold underline underline-offset-8 hover:scale-110 transition duration-100">
+                        {"Software Engineer & CS Student"}
+                    </h2>
 
-                // box with background canvas that ignores other elements and is always behind everything
-                <div class="absolute z-[0] overflow-hidden">
-                    <ParticleAnimation />
-                </div>
+                    <div class="flex flex-row justify-center p-5">
 
-                // here we want 2 boxes for the right and left sides
-                <div class="grid grid-cols-2 grid-flow-row p-6 h-full z-[1] relative">
-                    // left box
-                    <div class="float-left text-white  nueue-montreal-regular col-span-1">
-                        <div class="text-4xl hover:underline"> {"Kival Mahadew"} </div>
-                        <div class="text-3xl mt-2 hover:underline underline-offset-2">
-                            {"CS Student & Software Developer"}
-                        </div>
+                        <a href="https://github.com/kivalm" target="_blank"
+                            class="mx-2 hover:scale-110 transition duration-75">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                                class="bi bi-github" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                            </svg>
+                        </a>
 
-                        <div class="text-xl mt-4 hover:text-violet-400 transition-all" > {"- Home"} </div>
-                        <div class="text-xl hover:text-violet-400 transition-all" onclick={navigate(Route::Blog)}> {"- Blog"} </div>
-                        <div class="text-xl hover:text-violet-400 transition-all" onclick={navigate(Route::Projects)}> {"- Projects"} </div>
-                    </div>
-
-                    <div class="text-white text-xl nueue-montreal-regular col-span-1  flex flex-col justify-end">
-                        <div class="flex flex-row justify-end">
-                            <div class="flex flex-col justify-end">
-                                // github icon
-                                <div>
-                                    <a class="inline-flex gap-2" href="mailto:kivalm@protonmail.com"
-                                        referrer_policy="no-referrer" target="_blank">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                            class="bg-black text-white fill-current rounded-lg">
-                                            <path
-                                                d="M23 0l-4.5 16.5-6.097-5.43 5.852-6.175-7.844 5.421-5.411-1.316 18-9zm-11 12.501v5.499l2.193-3.323-2.193-2.176zm-8.698 6.825l-1.439-.507 5.701-5.215 1.436.396-5.698 5.326zm3.262 4.287l-1.323-.565 4.439-4.503 1.32.455-4.436 4.613zm-4.083.387l-1.481-.507 8-7.89 1.437.397-7.956 8z" />
-                                        </svg>
-                                        <span class="hover:text-violet-400 transition-all"> {"Email"} </span>
-                                    </a>
-                                </div>
-
-                                <div>
-                                    <a class="inline-flex gap-2" href="https://www.linkedin.com/in/kivalm/"
-                                        referrer_policy="no-referrer" target="_blank">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                            class="bg-black text-white fill-current rounded-lg">
-
-                                            <path
-                                                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-                                        </svg>
-                                        <span class="hover:text-violet-400 transition-all"> {"LinkedIn"} </span>
-                                    </a>
-                                </div>
-
-                                <div>
-                                    <a class="inline-flex gap-2" href="https://github.com/kivalm/"
-                                        referrer_policy="no-referrer" target="_blank">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                            class="bg-black text-white fill-current rounded-lg">
-                                            <path
-                                                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                                        </svg>
-                                        <span class="hover:text-violet-400 transition-all"> {"Github"} </span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+                        <a href="https://www.linkedin.com/in/kivalm/" target="_blank"
+                            class="mx-2 hover:scale-110 transition duration-75">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                                class="bi bi-linkedin" viewBox="0 0 16 16">
+                                <path
+                                    d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+                            </svg>
+                        </a>
+                        <a href="/" class="mx-2 hover:scale-125 transition-all duration-75">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                                class="animate-slide animate-linear " viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                            </svg>
+                        </a>
                     </div>
                 </div>
             </div>
-        </div>
+        </header>
+
     </>
-      }
+    }
 }
--- src/components/other/particle_animation.rs
+++ src/components/other/particle_animation.rs
@@ -1,240 +1,238 @@
-use gloo_timers::callback::Timeout;
-use js_sys::Math;
-use wasm_bindgen::{JsCast, JsValue};
-use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
-use yew::prelude::*;
+// use gloo_timers::callback::Timeout;
+// use js_sys::Math;
+// use wasm_bindgen::{JsCast, JsValue};
+// use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
+// use yew::prelude::*;
 
-use crate::components::index::MOUSE_POS;
+// pub struct ParticleAnimation {
+//     canvas: Option<CanvasRenderingContext2d>,
+//     generator: ParticleGenerator,
+//     offset: (u32, u32),
+//     width: u32,
+//     height: u32,
+// }
 
-pub struct ParticleAnimation {
-    canvas: Option<CanvasRenderingContext2d>,
-    generator: ParticleGenerator,
-    offset: (u32, u32),
-    width: u32,
-    height: u32,
-}
+// pub enum Msg {
+//     Render,
+// }
 
-pub enum Msg {
-    Render,
-}
+// impl Component for ParticleAnimation {
+//     type Message = Msg;
+//     type Properties = ();
 
-impl Component for ParticleAnimation {
-    type Message = Msg;
-    type Properties = ();
+//     fn create(_: &Context<Self>) -> Self {
+//         let document = web_sys::window().unwrap().document().unwrap();
+//         let main_box = document.get_element_by_id("main-box").unwrap();
 
-    fn create(_: &Context<Self>) -> Self {
-        let document = web_sys::window().unwrap().document().unwrap();
-        let main_box = document.get_element_by_id("main-box").unwrap();
+//         let height = main_box.client_height();
+//         let width = main_box.client_width();
 
-        let height = main_box.client_height();
-        let width = main_box.client_width();
+//         let div = main_box.dyn_into::<web_sys::HtmlDivElement>().unwrap();
+//         let offset_top = div.offset_top();
+//         let offset_left = div.offset_left();
 
-        let div = main_box.dyn_into::<web_sys::HtmlDivElement>().unwrap();
-        let offset_top = div.offset_top();
-        let offset_left = div.offset_left();
+//         Self {
+//             canvas: None,
+//             generator: ParticleGenerator::new(
+//                 width.try_into().unwrap(),
+//                 height.try_into().unwrap(),
+//             ),
+//             width: width.try_into().unwrap(),
+//             height: height.try_into().unwrap(),
+//             offset: (
+//                 offset_left.try_into().unwrap(),
+//                 offset_top.try_into().unwrap(),
+//             ),
+//         }
+//     }
 
-        Self {
-            canvas: None,
-            generator: ParticleGenerator::new(
-                width.try_into().unwrap(),
-                height.try_into().unwrap(),
-            ),
-            width: width.try_into().unwrap(),
-            height: height.try_into().unwrap(),
-            offset: (
-                offset_left.try_into().unwrap(),
-                offset_top.try_into().unwrap(),
-            ),
-        }
-    }
+//     fn view(&self, _: &Context<Self>) -> Html {
+//         let width = self.width.to_string();
+//         let height = self.height.to_string();
 
-    fn view(&self, _: &Context<Self>) -> Html {
-        let width = self.width.to_string();
-        let height = self.height.to_string();
+//         html! {
+//             <canvas id="canvas" width={width} height={height}>
+//                 {"Your browser does not support the HTML5 canvas tag."}
+//             </canvas>
+//         }
+//     }
 
-        html! {
-            <canvas id="canvas" width={width} height={height}>
-                {"Your browser does not support the HTML5 canvas tag."}
-            </canvas>
-        }
-    }
+//     fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
+//         if first_render {
+//             let document = web_sys::window().unwrap().document().unwrap();
+//             let canvas = document.get_element_by_id("canvas").unwrap();
+//             let canvas: HtmlCanvasElement = canvas.dyn_into().unwrap();
+//             let context = canvas.get_context("2d").unwrap().unwrap();
+//             let context: CanvasRenderingContext2d = context.dyn_into().unwrap();
+//             self.canvas = Some(context);
 
-    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
-        if first_render {
-            let document = web_sys::window().unwrap().document().unwrap();
-            let canvas = document.get_element_by_id("canvas").unwrap();
-            let canvas: HtmlCanvasElement = canvas.dyn_into().unwrap();
-            let context = canvas.get_context("2d").unwrap().unwrap();
-            let context: CanvasRenderingContext2d = context.dyn_into().unwrap();
-            self.canvas = Some(context);
+//             let particles = {
+//                 let density = 0.001;
+//                 let square = self.width * self.height;
+//                 (square as f64 * density) as usize
+//             };
+//             self.generator.add_particles(particles.try_into().unwrap());
 
-            let particles = {
-                let density = 0.001;
-                let square = self.width * self.height;
-                (square as f64 * density) as usize
-            };
-            self.generator.add_particles(particles.try_into().unwrap());
+//             // start rendering
+//             ctx.link().send_message(Msg::Render);
+//         }
+//     }
 
-            // start rendering
-            ctx.link().send_message(Msg::Render);
-        }
-    }
+//     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+//         match msg {
+//             Msg::Render => {
+//                 // log::info!("rendering");
+//                 web_sys::console::log_1(&JsValue::from_str("rendering"));
+//                 let context = self.canvas.as_ref().unwrap();
 
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::Render => {
-                // log::info!("rendering");
-                web_sys::console::log_1(&JsValue::from_str("rendering"));
-                let context = self.canvas.as_ref().unwrap();
+//                 context.clear_rect(0.0, 0.0, self.width.into(), self.height.into());
 
-                context.clear_rect(0.0, 0.0, self.width.into(), self.height.into());
+//                 let mouse = unsafe { MOUSE_POS };
 
-                let mouse = unsafe { MOUSE_POS };
+//                 let mouse = (
+//                     mouse.0 - self.offset.0 as i32,
+//                     mouse.1 - self.offset.1 as i32,
+//                 );
 
-                let mouse = (
-                    mouse.0 - self.offset.0 as i32,
-                    mouse.1 - self.offset.1 as i32,
-                );
+//                 self.generator.next(mouse.0.into(), mouse.1.into());
+//                 let particles = self.generator.points();
+//                 for (x, y) in particles {
+//                     // dots at x, y
+//                     context.begin_path();
+//                     context
+//                         .arc(x as f64, y as f64, 1.0, 0.0, 2.0 * std::f64::consts::PI)
+//                         .unwrap();
+//                     context.set_fill_style(&JsValue::from_str("white"));
+//                     context.fill();
+//                 }
 
-                self.generator.next(mouse.0.into(), mouse.1.into());
-                let particles = self.generator.points();
-                for (x, y) in particles {
-                    // dots at x, y
-                    context.begin_path();
-                    context
-                        .arc(x as f64, y as f64, 1.0, 0.0, 2.0 * std::f64::consts::PI)
-                        .unwrap();
-                    context.set_fill_style(&JsValue::from_str("white"));
-                    context.fill();
-                }
+//                 let link = ctx.link().clone();
 
-                let link = ctx.link().clone();
+//                 let fps = 60;
+//                 let interval = 1000 / fps;
 
-                let fps = 60;
-                let interval = 1000 / fps;
+//                 let timeout = Timeout::new(interval, move || {
+//                     link.send_message(Msg::Render);
+//                 });
 
-                let timeout = Timeout::new(interval, move || {
-                    link.send_message(Msg::Render);
-                });
+//                 timeout.forget();
 
-                timeout.forget();
+//                 false
+//             }
+//         }
+//     }
+// }
 
-                false
-            }
-        }
-    }
-}
+// struct Particle {
+//     x: f64,
+//     y: f64,
+//     vx: f64,
+//     vy: f64,
+// }
 
-struct Particle {
-    x: f64,
-    y: f64,
-    vx: f64,
-    vy: f64,
-}
+// struct ParticleGenerator {
+//     particles: Vec<Particle>,
+//     width: f64,
+//     height: f64,
+// }
 
-struct ParticleGenerator {
-    particles: Vec<Particle>,
-    width: f64,
-    height: f64,
-}
+// impl ParticleGenerator {
+//     fn new(width: u32, height: u32) -> Self {
+//         Self {
+//             particles: Vec::new(),
+//             width: width.into(),
+//             height: height.into(),
+//         }
+//     }
 
-impl ParticleGenerator {
-    fn new(width: u32, height: u32) -> Self {
-        Self {
-            particles: Vec::new(),
-            width: width.into(),
-            height: height.into(),
-        }
-    }
+//     fn add_particles(&mut self, n: u32) {
+//         for _ in 0..n {
+//             let x = Math::random() * self.width;
+//             let y = Math::random() * self.height;
+//             let vx = Math::random() * 2.0 - 1.0;
+//             let vy = Math::random() * 2.0 - 1.0;
 
-    fn add_particles(&mut self, n: u32) {
-        for _ in 0..n {
-            let x = Math::random() * self.width;
-            let y = Math::random() * self.height;
-            let vx = Math::random() * 2.0 - 1.0;
-            let vy = Math::random() * 2.0 - 1.0;
+//             if vx < 0.5 && vx > -0.5 {
+//                 // make sure the particle is moving
+//                 if vx < 0.0 {
+//                     -0.5
+//                 } else {
+//                     0.5
+//                 };
+//             }
 
-            if vx < 0.5 && vx > -0.5 {
-                // make sure the particle is moving
-                if vx < 0.0 {
-                    -0.5
-                } else {
-                    0.5
-                };
-            }
+//             if vy < 0.5 && vy > -0.5 {
+//                 // make sure the particle is moving
+//                 if vy < 0.0 {
+//                     -0.5
+//                 } else {
+//                     0.5
+//                 };
+//             }
 
-            if vy < 0.5 && vy > -0.5 {
-                // make sure the particle is moving
-                if vy < 0.0 {
-                    -0.5
-                } else {
-                    0.5
-                };
-            }
+//             self.particles.push(Particle { x, y, vx, vy });
+//         }
+//     }
 
-            self.particles.push(Particle { x, y, vx, vy });
-        }
-    }
+//     fn simulation(&mut self) {
+//         for particle in self.particles.iter_mut() {
+//             particle.x += particle.vx;
+//             particle.y += particle.vy;
 
-    fn simulation(&mut self) {
-        for particle in self.particles.iter_mut() {
-            particle.x += particle.vx;
-            particle.y += particle.vy;
+//             let elasticity = 0.9;
 
-            let elasticity = 0.9;
+//             // bounce off the walls
+//             if particle.x < 0.0 {
+//                 particle.x = -particle.x;
+//                 particle.vx = -particle.vx;
+//                 particle.vx *= elasticity;
+//             } else if particle.x >= self.width {
+//                 particle.x = self.width - (particle.x - self.width);
+//                 particle.vx = -particle.vx;
+//                 particle.vx *= elasticity;
+//             }
 
-            // bounce off the walls
-            if particle.x < 0.0 {
-                particle.x = -particle.x;
-                particle.vx = -particle.vx;
-                particle.vx *= elasticity;
-            } else if particle.x >= self.width {
-                particle.x = self.width - (particle.x - self.width);
-                particle.vx = -particle.vx;
-                particle.vx *= elasticity;
-            }
+//             if particle.y < 0.0 {
+//                 particle.y = -particle.y;
+//                 particle.vy = -particle.vy;
+//                 particle.vy *= elasticity;
+//             } else if particle.y > self.height {
+//                 particle.y = self.height - (particle.y - self.height);
+//                 particle.vy = -particle.vy;
+//                 particle.vy *= elasticity;
+//             }
+//         }
+//     }
 
-            if particle.y < 0.0 {
-                particle.y = -particle.y;
-                particle.vy = -particle.vy;
-                particle.vy *= elasticity;
-            } else if particle.y > self.height {
-                particle.y = self.height - (particle.y - self.height);
-                particle.vy = -particle.vy;
-                particle.vy *= elasticity;
-            }
-        }
-    }
+//     fn next(&mut self, x: f64, y: f64) {
+//         // do all the normal stuff
+//         self.simulation();
 
-    fn next(&mut self, x: f64, y: f64) {
-        // do all the normal stuff
-        self.simulation();
+//         // handle the mouses
+//         self.handle_attraction(x, y);
+//     }
 
-        // handle the mouses
-        self.handle_attraction(x, y);
-    }
+//     fn handle_attraction(&mut self, x: f64, y: f64) {
+//         // particles will gain velocity towards the mouse
+//         let attraction = 0.1;
 
-    fn handle_attraction(&mut self, x: f64, y: f64) {
-        // particles will gain velocity towards the mouse
-        let attraction = 0.1;
+//         for particle in self.particles.iter_mut() {
+//             let dx = x - particle.x;
+//             let dy = y - particle.y;
+//             let distance = (dx * dx + dy * dy).sqrt();
 
-        for particle in self.particles.iter_mut() {
-            let dx = x - particle.x;
-            let dy = y - particle.y;
-            let distance = (dx * dx + dy * dy).sqrt();
+//             if distance < 100.0 {
+//                 let angle = dy.atan2(dx);
+//                 particle.vx += attraction * angle.cos();
+//                 particle.vy += attraction * angle.sin();
+//             }
+//         }
+//     }
 
-            if distance < 100.0 {
-                let angle = dy.atan2(dx);
-                particle.vx += attraction * angle.cos();
-                particle.vy += attraction * angle.sin();
-            }
-        }
-    }
-
-    fn points(&self) -> Vec<(i32, i32)> {
-        self.particles
-            .iter()
-            .map(|p| (p.x as i32, p.y as i32))
-            .collect()
-    }
-}
+//     fn points(&self) -> Vec<(i32, i32)> {
+//         self.particles
+//             .iter()
+//             .map(|p| (p.x as i32, p.y as i32))
+//             .collect()
+//     }
+// }
