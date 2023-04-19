27019bbe9af825f6af463d4c55ce137db3e816e6
Kival M <kivalm@protonmail.com>
2023-04-19 13:15:00 +0200

    responsive nav

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -1,7 +1,8 @@
-use yew::{function_component, html, Html, Properties};
-use yew_router::prelude::{use_route, Link};
-
 use crate::router::Route;
+use wasm_bindgen::UnwrapThrowExt;
+use web_sys::{window, Document};
+use yew::{function_component, html, Callback, Html};
+use yew_router::prelude::{use_route, Link};
 
 // #[derive(Clone, PartialEq, Properties)]
 // pub struct NavbarProps {
@@ -15,26 +16,35 @@ pub fn navbar() -> Html {
     let standard_classes = "comfortaa font-black text-white";
     let active_classes = "comfortaa font-black text-[#E27D60]";
 
+    let toggle_menu_cb = Callback::from(move |_| {
+        let document = window()
+            .expect_throw("window is undefined")
+            .document()
+            .expect_throw("document is undefined");
+
+        let el = document.get_element_by_id("nav-items").unwrap_throw();
+        let class_list = el.class_list();
+        class_list.toggle("hidden").unwrap_throw();
+    });
+
     html! {
     <>
-        <div class="md:hidden flex items-center justify-center w-1/12">
-            <svg class="w-6 h-6 text-[#E27D60]" fill="none" stroke-linecap="round" stroke-linejoin="round"
-                stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                <path d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-        </div>
-        <nav class="bg-[#8338ec] p-5 hidden md:flex">
-            <Link<Route> to={Route::Home}>
-                <div class="comfortaa font-black text-white"> {"Kival M."} </div>
-            </Link<Route>>
 
-            <div class="flex flex-row gap-5 ml-auto">
+        <nav class="bg-[#8338ec] p-5 flex flex-col md:flex-row">
 
+            <div class="w-full flex justify-between">
                 <Link<Route> to={Route::Home}>
-                    <div class={ if current_route==Route::Home { active_classes } else { standard_classes } }> {"Home"}
-                    </div>
+                    <div class="comfortaa font-black text-white"> {"Kival M."} </div>
                 </Link<Route>>
+                <div class="flex-row gap-5 ml-auto flex md:hidden cursor-pointer" onclick={toggle_menu_cb}>
+                    <svg class="w-6 h-6 text-white" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <path d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </div>
+            </div>
 
+            <div class="hidden flex-col md:ml-auto md:flex md:gap-5 md:flex-row" id="nav-items">
                 <Link<Route> to={Route::Blog}>
                     <div class={ if current_route==Route::Blog { active_classes } else { standard_classes } }> {"Blog"}
                     </div>
--- src/routes/cv/mod.rs
+++ src/routes/cv/mod.rs
@@ -1,13 +1,10 @@
-pub mod models;
-
-use yew::{function_component, html, Html};
-use yew_router::prelude::Link;
-
 use crate::{
     components::navbar::Navbar,
-    router::Route,
     routes::cv::models::{EDUCATION, WORK},
 };
+use yew::{function_component, html, Html};
+
+pub mod models;
 
 #[function_component(CVPage)]
 pub fn cv() -> Html {
@@ -23,11 +20,11 @@ pub fn cv() -> Html {
                 {"Software Engineer & CS Student"}
             </div>
             <h3 class="text-lg flex items-center gap-1 text-gray-600">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope"
-                viewBox="0 0 16 16">
-                <path
-                    d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
-            </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+                </svg>
                 {"kivalm(at)protonmail.com"}
             </h3>
             <h3 class="text-lg flex items-center gap-1 text-gray-600">
@@ -46,18 +43,16 @@ pub fn cv() -> Html {
         <div class="grid grid-cols-1 lg:grid-cols-2 container mx-auto">
 
             <div>
-               <h3 class="font-bold text-2xl roboto"> {"Education"} </h3>
-              {EDUCATION.iter().map(|f| f.to_html()).collect::<Html>()}
+                <h3 class="font-bold text-2xl roboto"> {"Education"} </h3>
+                {EDUCATION.iter().map(|f| f.to_html()).collect::<Html>()}
             </div>
 
             <div>
-              <h3 class="font-bold text-2xl roboto"> {"Experience"} </h3>
-              {WORK.iter().map(|f| f.to_html()).collect::<Html>()}
+                <h3 class="font-bold text-2xl roboto"> {"Experience"} </h3>
+                {WORK.iter().map(|f| f.to_html()).collect::<Html>()}
             </div>
 
         </div>
-
-
 
     </>
     }
